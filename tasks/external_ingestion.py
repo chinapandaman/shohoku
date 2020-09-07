53fa36e8e454cfa8d0641b9408ba60e9ef8fba34
chinapandaman <b.j.li@foxmail.com>
2020-09-07 14:25:43 -0500

    scratch external ingestion

---
+++ tasks/external_ingestion.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+
+import datetime
+import json
+
+import requests
+from pydal import DAL, Field
+
+
+class ExternalIngestion(object):
+    def __init__(self):
+        self.db = DAL("sqlite:memory")
+
+        self.db.define_table(
+            "nba_events",
+            Field("event_datetime", type="datetime"),
+            Field("event_home_team", length=64),
+            Field("event_away_team", length=64),
+        )
+
+        self.event_id = 4387
+        self.api = "https://www.thesportsdb.com/api/v1/json/1/eventsnextleague.php?id={}".format(
+            self.event_id
+        )
+
+    def load_events(self):
+        db = self.db
+
+        api_response = requests.get(self.api)
+
+        if api_response.status_code == 200:
+            events = json.loads(api_response.content)["events"]
+
+            for each in events:
+                db.nba_events.insert(
+                    **{
+                        "event_datetime": datetime.datetime.strptime(
+                            "{} {}".format(each["dateEvent"], each["strTime"]),
+                            "%Y-%m-%d %H:%M:%S",
+                        ),
+                        "event_home_team": each["strHomeTeam"],
+                        "event_away_team": each["strAwayTeam"],
+                    }
+                )
+
+        db.commit()
+
+
+if __name__ == "__main__":
+    ExternalIngestion().load_events()
